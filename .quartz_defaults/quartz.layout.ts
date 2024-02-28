c019d980beab7118e7324f637a57b896b7cf3945
MarvinLeiers <mxrvn@leiers.org>
2024-02-28 22:04:41 +0100

    change footer link

--- .quartz_defaults/quartz.layout.ts
+++ .quartz_defaults/quartz.layout.ts
@@ -7,7 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/MarvinLeiers",
+      GitHub: "https://github.com/MarvinLeiers/uni",
     },
   }),
 }
